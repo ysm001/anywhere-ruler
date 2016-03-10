e56e4e629ffc16880e19385ec64f5db07b0953aa
ysm001 <ry.ysd01@gmail.com>
2016-03-10 16:45:51 +0900

    EntryPointをHomeからAppに変更

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,13 @@ import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('main')
+);
